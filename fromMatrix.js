bcbf680c32f751a5e046831a26d9407ae9243a00
Mikko Haapoja <me@mikkoh.com>
2015-06-01 14:36:57 -0400

    Added perspective function to transform for browsers which support transforms because it seems like the theres an bug in rednering transforms on webkit browsers

--- fromMatrix.js
+++ fromMatrix.js
@@ -6,6 +6,7 @@ var applyTransformOrigin = require('./lib/applyTransformOrigin');
 module.exports = function ieCSSTransformFromMatrix(el, matrix, transformOrigin) {
 
     var matrixString;
+    var perspective;
 
     if(checkIsCrappyIE(el)) {
       applyMatrix(el, matrix, transformOrigin);
@@ -15,7 +16,18 @@ module.exports = function ieCSSTransformFromMatrix(el, matrix, transformOrigin) 
       if(matrix.length === 6) {
         el.style.transform = 'matrix(' + matrixString + ')';  
       } else {
-        el.style.transform = 'matrix3d(' + matrixString + ')';  
+
+        perspective = matrix[ 11 ];
+
+        // the following is done because it seems like webkit does not render
+        // the matrix perspective value correctly
+        if(perspective) {
+          perspective = -1 / perspective;
+
+          el.style.transform = 'perspective(' + perspective + 'px) matrix3d(' + matrixString + ')';  
+        } else {
+          el.style.transform = 'matrix3d(' + matrixString + ')';    
+        }
       }
 
       applyTransformOrigin(el, transformOrigin);
